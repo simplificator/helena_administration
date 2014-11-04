277866819028556638098383e9073b97aa17b974
Markus Graf <info@markusgraf.ch>
2014-11-04 14:58:37 +0100

    run with own layout

--- app/controllers/helena_administration/application_controller.rb
+++ app/controllers/helena_administration/application_controller.rb
@@ -1,7 +1,13 @@
 module HelenaAdministration
-  class ApplicationController < ::ApplicationController
+  class ApplicationController < ActionController::Base
+    before_action :set_locale
+
     def add_breadcrumb(name, path = nil, options = {})
       super(name.presence || t('shared.untitled'), path, options)
+    end
+
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
     end
   end
 end
--- lib/helena_administration/engine.rb
+++ lib/helena_administration/engine.rb
@@ -2,6 +2,6 @@ module HelenaAdministration
   class Engine < ::Rails::Engine
     isolate_namespace HelenaAdministration
     config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    config.assets.precompile += %w( helena_administration.js )
+    config.assets.precompile += %w( helena_administration.js helena_administration.css  )
   end
 end
