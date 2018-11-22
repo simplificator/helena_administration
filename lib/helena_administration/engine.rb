a0eee4134e9da17ee759447c6c9f941ea9cad7fa
Markus Graf <info@markusgraf.ch>
2018-11-22 14:13:52 +0100

    Remove unused code

--- lib/helena_administration/engine.rb
+++ lib/helena_administration/engine.rb
@@ -4,10 +4,11 @@ module HelenaAdministration
     config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.assets.precompile += %w[helena_administration.js helena_administration.css]
 
-    config.to_prepare do
-      Dir.glob(Rails.root + 'app/decorators/**/*_decorator*.rb').each do |c|
-        require_dependency c
-      end
-    end
+    # Thre are no decorators at the moment
+    # config.to_prepare do
+    #   Dir.glob(Rails.root + 'app/decorators/**/*_decorator*.rb').each do |c|
+    #     require_dependency c
+    #   end
+    # end
   end
 end
