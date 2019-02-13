d7c5019b3c741a882a3d2192950cdfd92ab72faa
Markus Graf <info@markusgraf.ch>
2019-02-13 10:45:49 +0100

    Fixed missing codemirror.css in assests.

--- lib/helena_administration/engine.rb
+++ lib/helena_administration/engine.rb
@@ -2,7 +2,7 @@ module HelenaAdministration
   class Engine < ::Rails::Engine
     isolate_namespace HelenaAdministration
     config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    config.assets.precompile += %w[helena_administration.js helena_administration.css]
+    config.assets.precompile += %w[codemirror* codemirror/**/* polyfills.js helena_administration.css helena_administration.js helena_administration.css]
 
     # Thre are no decorators at the moment
     # config.to_prepare do
